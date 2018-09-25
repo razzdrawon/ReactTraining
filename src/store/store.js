72bef2dfb3e465a89f8d47e8e0b30ff6c68a57de
Ricardo Axel Padilla Gonzalez <Ricardo.Padilla@walmart.com>
2018-09-25 10:30:51 -0500

    switch case

--- src/store/store.js
+++ src/store/store.js
@@ -4,18 +4,29 @@ import * as user from '../user.json';
 const reducer = (state, action) => {
     console.log(action);
 
-    if( action.type === "SET_EMAIL") {
-        const newState = {...state};
-        newState.person.email = action.email;
-        return newState;    
-    }
-    if( action.type === "SET_CELL") {
-        const newState = {...state};
-        newState.person.cell = action.cell;
-        return newState;    
+    if(!action) return state;
+
+    switch (action.type) {
+        case "SET_EMAIL":
+            return {...state, person: {...state.person, email: action.emal}};
+        case "SET_CELL":
+            return {...state, person: {...state.person, cell: action.emacelll}};
+        default:
+            return state;
     }
 
-    return state;
+    // if( action.type === "SET_EMAIL") {
+    //     const newState = {...state};
+    //     newState.person.email = action.email;
+    //     return newState;    
+    // }
+    // if( action.type === "SET_CELL") {
+    //     const newState = {...state};
+    //     newState.person.cell = action.cell;
+    //     return newState;    
+    // }
+
+    // return state;
 };
 const initialState = {person: user};
 
