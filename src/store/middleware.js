fbae88c90963540198ae446caa4b9d765ffee8ea
Ricardo Axel Padilla Gonzalez <Ricardo.Padilla@walmart.com>
2018-09-25 15:53:03 -0500

    fetch city and state

--- src/store/middleware.js
+++ src/store/middleware.js
@@ -1,4 +1,6 @@
-import { actionTypes } from "./actions";
+import { actions, actionTypes } from "./actions";
+
+
 
 export const loggingMiddleware = ({getState, dispatch}) => next => action => {
   console.log(action.type, getState())
@@ -8,10 +10,21 @@ export const loggingMiddleware = ({getState, dispatch}) => next => action => {
 
 export const fetchCityAndStateMiddleware = ({getState, dispatch}) => next => action => {
   console.log(action.type, getState())
+  const url = `http://api.zippopotam.us/mx/${action.postCode}`;
 
   if(action.type === actionTypes.FETCH_CITY_AND_STATE)
   {
     console.log(action.type);
+    console.log(action.postCode);
+    fetch(url)
+    .then(res => res.json())
+    .then(postCodeObj => {
+      console.log(postCodeObj);
+
+      dispatch(actions.setState(postCodeObj.places[0].state));
+      dispatch(actions.setCity(postCodeObj.places[0]['place name']));
+    })
+
   }
   next(action);
   console.warn(action.type, getState())
